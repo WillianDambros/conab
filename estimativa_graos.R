34c63aa0f92c5bd21e8d23c978735f378c195c09
WillianDambros <126815136+WillianDambros@users.noreply.github.com>
2026-03-17 16:43:17 -0400

    Update estimativa_graos.R

--- estimativa_graos.R
+++ estimativa_graos.R
@@ -21,6 +21,33 @@ arquivo <- arquivo |>
     producao_t = producao_mil_t * 1000
   )
 
+?stringr::str_trim()
+arquivo |> dplyr::glimpse()
+arquivo$id_levantamento |> unique()
+
+arquivo <- arquivo |>
+  dplyr::mutate(n_levantamento = as.integer(stringr::str_trim(id_levantamento)),
+                mes = dplyr::case_when(
+                  n_levantamento == 1  ~ 10,
+                  n_levantamento == 2  ~ 11,
+                  n_levantamento == 3  ~ 12,
+                  n_levantamento == 4  ~ 1,
+                  n_levantamento == 5  ~ 2,
+                  n_levantamento == 6  ~ 3,
+                  n_levantamento == 7  ~ 4,
+                  n_levantamento == 8  ~ 5,
+                  n_levantamento == 9  ~ 6,
+                  n_levantamento == 10 ~ 7,
+                  n_levantamento == 11 ~ 8,
+                  n_levantamento == 12 ~ 9
+                ),
+                ano_levantamento = ifelse(n_levantamento <= 3,
+                                          lubridate::year(ano_referencia) - 1,
+                                          lubridate::year(ano_referencia)),
+                data_levantamento = lubridate::make_date(
+                  ano_levantamento, mes, 1)) |>
+  dplyr::select(!c(mes, ano_levantamento))
+
 source("X:/POWER BI/NOVOCAGED/conexao.R")
 
 RPostgres::dbListTables(conexao)
